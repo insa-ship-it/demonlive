6206dcd84edd6b898743f7ddea50188e1500bba8
insa-ship-it <leemichaelinsa@gmail.com>
2026-06-24 21:34:09 -0500

    Update combine_remote_playlists_with_epg.py

--- combine_remote_playlists_with_epg.py
+++ combine_remote_playlists_with_epg.py
@@ -21,7 +21,6 @@ PLAYLISTS = [
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/pluto_us.m3u", 
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/pluto_gb.m3u", 
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/ott-news.m3u8", 
-    "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/drew.m3u8", 
     # Add more playlists here in the format: "URL_TO_PLAYLIST"
 ]
 
